aaf9a8576eb83dcebff6510437f45ba5d8bf9c51
Alex Baryzhikov <aleksiarts@gmail.com>
2018-11-04 19:27:37 +0300

    Feature: ScopedAppActivity

--- KotlinPlayground/app/src/main/java/com/alexbaryzhikov/kotlinplayground/MainActivity.kt
+++ KotlinPlayground/app/src/main/java/com/alexbaryzhikov/kotlinplayground/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.alexbaryzhikov.kotlinplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.alexbaryzhikov.kotlinplayground.coroutines.AuthUser
+import com.alexbaryzhikov.kotlinplayground.coroutines.ScopedAppActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import kotlin.coroutines.ContinuationInterceptor
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ScopedAppActivity() {
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        launch {
+            Log.i(TAG, "CoroutineName: ${coroutineContext[CoroutineName]}")
+            Log.i(TAG, "ContinuationInterceptor: ${coroutineContext[ContinuationInterceptor]}")
+            Log.i(TAG, "Job: ${coroutineContext[Job]}")
+            Log.i(TAG, "CoroutineExceptionHandler: ${coroutineContext[CoroutineExceptionHandler]}")
+            Log.i(TAG, "AuthUser: ${coroutineContext[AuthUser]}")
+            delay(2000)
+            textView.text = getString(R.string.coroutine_text)
+        }
+        textView.text = getString(R.string.sequential_text)
     }
 }
