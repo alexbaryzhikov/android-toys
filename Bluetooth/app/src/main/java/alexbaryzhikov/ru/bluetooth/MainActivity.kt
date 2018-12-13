08d2f8f368e27deac71435aae3978c613bef9817
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2018-12-13 18:08:10 +0300

    Project: Bluetooth

---
+++ Bluetooth/app/src/main/java/alexbaryzhikov/ru/bluetooth/MainActivity.kt
@@ -0,0 +1,12 @@
+package alexbaryzhikov.ru.bluetooth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
