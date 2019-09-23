447ff43ee57dd2a0bbe6865d9aa6de1dd2c15335
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-09-23 14:12:37 +0300

    Clean app data (need su or system app privileges)

--- ClearAppData/app/src/main/java/com/alexb/clearappdata/MainActivity.kt
+++ ClearAppData/app/src/main/java/com/alexb/clearappdata/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.alexb.clearappdata
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.io.File
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val scope: CoroutineScope = CoroutineScope(Job() + Dispatchers.IO)
+    private val tag = "CleanUpLog"
+    private val gmailUid = "com.google.android.gm"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,6 +23,32 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        scope.launch {
+            runCatching { cleanUpData2() }.onFailure {
+                Log.e(tag, "Error: $it")
+            }
+        }
+    }
 
+    private fun cleanUpData() {
+        val cache = cacheDir?.parentFile?.parentFile
+        require(cache != null) { "cache == null" }
+        val appDir = File(cache, gmailUid)
+        if (appDir.exists()) {
+            Log.d(tag, "Successfully found: $appDir")
+            val children = appDir.list()
+            children?.forEach { Log.d(tag, it) }
+        } else {
+            Log.e(tag, "File not found: $appDir")
+        }
+    }
+
+    private fun cleanUpData2() {
+        val c = Class.forName("android.app.ActivityManager")
+        val m = c.getMethod(
+            "clearApplicationUserData",
+            String::class.java/*, IPackageDataObserver::class.java */
+        )
+        m.invoke(c, gmailUid, null)
     }
 }
