1166fa93697882056a5ad8e44e7a74ef351c2d4d
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-10-24 18:00:17 +0300

    Knox sandbox: create container using more options

--- KnoxSandbox/app/src/main/java/ru/niisokb/mcc/framework/knox/container/ContainerCreator.kt
+++ KnoxSandbox/app/src/main/java/ru/niisokb/mcc/framework/knox/container/ContainerCreator.kt
@@ -1,19 +1,44 @@
 package ru.niisokb.mcc.framework.knox.container
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.text.TextUtils
+import android.util.Log
+import com.samsung.android.knox.container.CreationParams
 import com.samsung.android.knox.container.KnoxContainerManager
 import ru.niisokb.mcc.R
 import ru.niisokb.mcc.framework.utils.showToast
 
 class ContainerCreator {
 
+    private val warrantyBitOk: Boolean
+        get() = runCatching {
+            @SuppressLint("PrivateApi")
+            val clazz = Class.forName("android.os.SystemProperties")
+            val method = clazz.getDeclaredMethod("get", String::class.java)
+            method.isAccessible = true
+            var b = method.invoke(null, "ro.boot.warranty_bit") as String
+            if (TextUtils.isEmpty(b))
+                b = method.invoke(null, "ro.warranty_bit") as String
+            b == "0"
+        }.getOrDefault(false)
+
     fun createContainer(context: Context) {
+        Log.d(TAG, "Warranty bit ok: $warrantyBitOk")
         runCatching {
             val receiver = ContainerReceiver()
             receiver.register(context)
+            val creationParams = CreationParams().apply {
+                setConfigurationName(CONTAINER_TYPE)
+                setPasswordResetToken(PASSWORD_RESET_TOKEN)
+                setAdminPackageName(context.packageName)
+            }
+            Log.d(TAG, "Request container creation: $CONTAINER_TYPE")
 
-            val initialRequestId = KnoxContainerManager.createContainer("knox-b2b")
+            val initialRequestId = KnoxContainerManager.createContainer(creationParams)
+
             if (initialRequestId < 0) {
+                Log.e(TAG, "Container creation request failed, error code: $initialRequestId")
                 showToast(
                     context,
                     context.getString(R.string.container_creation_error, initialRequestId)
@@ -24,5 +49,11 @@ class ContainerCreator {
                 showToast(context, context.getString(R.string.container_creation_progress))
             }
         }
+    }
+
+    companion object {
+        private const val TAG = "ContainerCreator"
+        private const val CONTAINER_TYPE = "knox-b2b"
+        private const val PASSWORD_RESET_TOKEN = "soExR47kmmgdxUpWttBsq5hfcoZHDZM6k56VDQ04"
     }
 }
