01952ce0845e2b60fd1022fc63df1793e8c5eeb7
Alex Baryzhikov <abaryzhikov@niisokb.ru>
2019-11-01 13:54:14 +0300

    remove KotlinPlayground

--- KotlinPlayground/app/src/main/java/com/alexbaryzhikov/kotlinplayground/coroutines/AuthUser.kt
+++
@@ -1,9 +0,0 @@
-package com.alexbaryzhikov.kotlinplayground.coroutines
-
-import kotlin.coroutines.AbstractCoroutineContextElement
-import kotlin.coroutines.CoroutineContext
-
-/** Custom [CoroutineContext] element. */
-data class AuthUser(val name: String) : AbstractCoroutineContextElement(AuthUser) {
-    companion object Key : CoroutineContext.Key<AuthUser>
-}
